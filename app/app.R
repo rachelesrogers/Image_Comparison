a342738e1e59e92676777efc9a847a95c1c3c403
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2023-09-15 15:42:19 -0500

    adding scientist and judge svg

--- app/app.R
+++ app/app.R
@@ -25,11 +25,11 @@ ui <- fluidPage(
             #             min = 40,
             #             max = 150,
             #             value = 100),
-            # selectInput("clothes_choice", "Select Outfit:", 
-            #             choices=c("defendant", "forensic_scientist", "police",
-            #                       "judge", "analyst","inmate", "lawyer")),
-            # selectInput("head_choice", "Select Head:", 
-            #             choices=c("1", "2"))
+            selectInput("clothes_choice", "Select Outfit:",
+                        choices=c("defendant", "scientist", "police",
+                                  "judge", "analyst","inmate", "lawyer")),
+            selectInput("head_choice", "Select Head:",
+                        choices=c("1", "2"))
         ),
 
         # Show a plot of the generated distribution
@@ -41,6 +41,11 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  hand_location <- data.frame("character"=c("defendant", "scientist", "judge"), 
+                              "x1"=c(136, NA, 147), "y1"=c(180, NA, 195),
+                              "x2"=c(60, NA, 44),"y2"=c(188, NA, 197))
+  
     output$characterPlot <- renderImage({
       
       # head_path_eyes <- paste0("www/head",input$head_choice,"_eyes.png")
@@ -63,16 +68,25 @@ server <- function(input, output) {
       # 
       # img <- c(clothes_skin, clothes, head_skin, head, head_eyes)
       
-      head_image <- rsvg_raw("www/head1.svg")
+      head_path <- paste0("www/head",input$head_choice,".svg")
+      body_path <- paste0("www/",input$clothes_choice,".svg")
+      
+      head_image <- rsvg_raw(head_path)
       head_magic <- image_read(head_image)
-      head_magic <- image_fill(head_magic, 'none', point=geometry_point(5,5), fuzz=10)
+      head_magic <- image_fill(head_magic, 'none', point=geometry_point(5,5), fuzz=20)
       head_magic <- image_fill(head_magic, input$skin, point=geometry_point(87,77), fuzz=10)
       
-      body_image <- rsvg_raw("www/defendant.svg")
+      body_hands <- hand_location[hand_location$character==input$clothes_choice,]
+      
+      body_image <- rsvg_raw(body_path)
       body_magic <- image_read(body_image)
-      body_magic <- image_fill(body_magic, 'none', point=geometry_point(5,5), fuzz=10)
-      body_magic <- image_fill(body_magic, input$skin, point=geometry_point(136,180), fuzz=10)
-      body_magic <- image_fill(body_magic, input$skin, point=geometry_point(60,188), fuzz=10)
+      body_magic <- image_fill(body_magic, 'none', point=geometry_point(5,5), fuzz=20)
+      if (!is.na(body_hands$x1) & !is.na(body_hands$y1)){
+      body_magic <- image_fill(body_magic, input$skin, 
+                               point=geometry_point(body_hands$x1,body_hands$y1), fuzz=10)}
+      if (!is.na(body_hands$x2) & !is.na(body_hands$y2)){
+      body_magic <- image_fill(body_magic, input$skin, 
+                               point=geometry_point(body_hands$x2,body_hands$y2), fuzz=10)}
       
       img <- c(body_magic, head_magic)
       
