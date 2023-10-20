b412ccbe4f861d273b0ffa40ca136f1d614cf10e
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2023-10-20 10:14:26 -0500

    two more heads

--- app/app.R
+++ app/app.R
@@ -35,7 +35,7 @@ ui <- fluidPage(
                         choices=c("defendant", "scientist", "police",
                                   "judge", "analyst","inmate", "lawyer")),
             selectInput("head_choice", "Select Head:",
-                        choices=c("1", "2","3","5","6","8", "9"))
+                        choices=c("1", "2","3","4","5","6","7","8","9"))
         ),
 
         # Show a plot of the generated distribution
