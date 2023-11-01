b8d91c49c3df12409d4cf06b4ab603a9c351e021
rachelrogers-gbr <rachel.rogers@huskers.unl.edu>
2023-11-01 12:31:55 -0500

    adding head 12

--- app/app.R
+++ app/app.R
@@ -26,7 +26,7 @@ ui <- fluidPage(
                         choices=c("defendant", "scientist", "police",
                                   "judge", "analyst","inmate", "lawyer")),
             selectInput("head_choice", "Select Head:",
-                        choices=c("1", "2","3","4","5","6","7","8","9", "10", "11"))
+                        choices=c("1", "2","3","4","5","6","7","8","9", "10", "11", "12"))
         ),
 
         # Show a plot of the generated distribution
